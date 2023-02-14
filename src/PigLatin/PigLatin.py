f77fb9b889698e3fd2fb1c4becc7c4a28bec945f
Lem_Scoot <89105667+LemScoot@users.noreply.github.com>
2023-02-14 15:45:23 -0700

    Delete src/PigLatin directory

--- src/PigLatin/PigLatin.py
+++
@@ -1,8 +0,0 @@
-def piglatin(word):
-  for i in range(len(word)):
-    if word[i].lower() in "aeiou":
-      return word[i: ] + word[: i] + "ay"
-
-
-word = input("Enter the word: ")
-print(f"{word} --> {piglatin(word)}")
